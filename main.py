58b872d0564f9e041edb4f9eb66cd576cf889e02
nipun.chathuranga <nipun.chathuranga@ideahub.lk>
2025-10-26 11:37:49 +0530

    Remove missing values

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ file_path = "dataset/healthcare_dataset.csv"
 df = pd.read_csv(file_path)
 
 # 01. --- Remove duplicates based on specific columns ---
+# df = df.drop_duplicates()
 df = df.drop_duplicates(subset=['Name', 'Doctor', 'Hospital'], keep='first')
 
 drop_duplicate_file_path = "dataset/cleaned_duplicate_healthcare_dataset.csv"
@@ -15,7 +16,9 @@ print("✅ Cleaned dataset saved successfully after removing duplicates")
 
 
 # 02. --- Remove missing (NaN) values ---
-df = df.dropna()
+# df = df.dropna()
+df = df.dropna(subset=['Name', 'Doctor', 'Hospital'])
+
 drop_missing_value_file_path = "dataset/cleaned_missing_value_healthcare_dataset.csv"
 df.to_csv(drop_missing_value_file_path, index=False)
 print("✅ Cleaned dataset saved successfully after missing values.")
